e3c615cc3431c0353aa75e1047cee8cc10ea0326
GiselleBarbosa <g88barbosa@gmail.com>
2023-11-07 10:28:40 -0300

    fix: added title pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,18 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
+
 import { AboutComponent } from './about/about.component';
 import { LocationComponent } from './location/location.component';
+import { NgModule } from '@angular/core';
 import { ShopComponent } from './shop/shop.component';
 
-// TODO: #4. Define unique page titles
+// TODO: #4. Define unique page titles // FIXED
 const routes: Routes = [
-  { path: 'shop', component: ShopComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'locate', component: LocationComponent },
-  { path: '',   redirectTo: '/shop', pathMatch: 'full' },
+  {
+    path: 'shop',
+    component: ShopComponent,
+    title: 'Our Shop – a11y in Angular',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'Our Story - a11y in Angular',
+  },
+  {
+    path: 'locate',
+    component: LocationComponent,
+    title: 'Find Us - a11y in Angular',
+  },
+  { path: '', redirectTo: '/shop', pathMatch: 'full' },
   { path: '**', component: ShopComponent },
 ];
 
