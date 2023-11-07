796e95e89e53a920e312a7fc29bb66e37d7e6acd
GiselleBarbosa <g88barbosa@gmail.com>
2023-11-07 20:19:47 -0300

    fix: added control focus with FocusTrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { NgModule } from '@angular/core';
+
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { A11yModule } from '@angular/cdk/a11y';
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ColorPickerComponent } from './shop/color-picker/color-picker.component';
+import { ColorPickerDialogComponent } from './shop/color-picker/color-picker-dialog/color-picker-dialog.component';
+import { DumplingComponent } from './shop/dumpling/dumpling.component';
+import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { LocationComponent } from './location/location.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -28,14 +34,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { GoogleMapsModule } from '@angular/google-maps';
-
-import { AboutComponent } from './about/about.component';
-import { ColorPickerComponent } from './shop/color-picker/color-picker.component';
-import { ColorPickerDialogComponent } from './shop/color-picker/color-picker-dialog/color-picker-dialog.component';
-import { DumplingComponent } from './shop/dumpling/dumpling.component';
-import { LocationComponent } from './location/location.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { NgModule } from '@angular/core';
 import { ShopComponent } from './shop/shop.component';
 
 // TODO: #9. Add the power of @angular/cdk/a11y
@@ -64,8 +64,9 @@ import { ShopComponent } from './shop/shop.component';
     MatSliderModule,
     MatToolbarModule,
     GoogleMapsModule,
+    A11yModule,
   ],
   providers: [Title],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
